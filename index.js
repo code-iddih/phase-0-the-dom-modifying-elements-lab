897b8ec84346c6078e0156b2227cd90f1a8a4b29
code-iddih <idinyamwaya@gmail.com>
2024-07-06 22:34:52 +0300

    Modifying DOM elements

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ let newHeader = document.createElement('h1');
 // The 'newHeader' variable that points to the <h1> node has an id of 'victory'
 newHeader.id = 'victory';
 
-// the 'newHeader' variable that points to the <h1> node with an id of 'victory' ...
+// The 'newHeader' variable that points to the <h1> node with an id of 'victory' ...
 // has the text "YOUR-NAME is the champion" (with your name in place of YOUR-NAME) inside it
 newHeader.textContent = 'YOUR-NAME is the champion';
 
